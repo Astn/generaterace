5938c18dbeba1c9d2f9beca656dca294d481fe2c
Astn <austin.w.harris@gmail.com>
2015-04-09 13:17:00 -0600

    Fsharp - Basic streaming

--- StreamingFSharp/Program.fs
+++ StreamingFSharp/Program.fs
@@ -1,7 +1,54 @@
 ﻿open Newtonsoft.Json
 open System
+open FSharp.Data
+open FSharp.Control.Reactive
+open System.Reactive.Concurrency
+
+type chipRead = JsonProvider<""" {"bib":5808,"checkpoint":1,"time":"00:06:07.9190000"} """>
+
+let loadFileLines (path:string) =
+     new IO.StreamReader(path)
+     |> Seq.unfold (fun sr ->
+        match sr.ReadLine() with
+        | null -> sr.Dispose(); None
+        | str -> Some(str,sr))
+
+let stream fileLocation = 
+    if not <| System.IO.File.Exists fileLocation then
+        printfn "the file [%A] does not exist" fileLocation
+        1
+    else
+        printfn "loading %A" fileLocation
+
+        let reads = loadFileLines fileLocation
+                    |> Seq.map chipRead.Parse
+                    |> Observable.ofSeqOn TaskPoolScheduler.Default
+                    |> Observable.delay (TimeSpan.FromMilliseconds(100.0))
+
+        let groups = reads 
+                        |> Observable.groupBy (fun item -> item.Checkpoint)
+                        |> Observable.subscribe (fun group -> 
+                            group 
+                            |> Observable.take 8
+                            |> Observable.subscribe (fun item ->
+                                match item.Checkpoint with
+                                | i when i%2=0 ->Console.ForegroundColor <- ConsoleColor.Yellow
+                                | _ ->Console.ResetColor()
+                                printfn "chk: %i bib: %i at %A" item.Checkpoint item.Bib item.Time.TimeOfDay
+                                )
+                            |> ignore
+                        )       
+        Console.ReadLine() |> ignore
+        groups.Dispose()
+        0
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let file = 
+        match argv with
+        | args when args.Length = 1 -> args.[0]
+        | _ -> ""
+    
+    stream file
+
+    

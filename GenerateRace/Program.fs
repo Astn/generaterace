2da260e6263cb45eda1ec34f446f77187d3750e5
Astn <austin.w.harris@gmail.com>
2015-04-08 20:54:13 -0600

    a little cleanup

--- GenerateRace/Program.fs
+++ GenerateRace/Program.fs
@@ -1,46 +1,71 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿open Newtonsoft.Json
+open System
+
+// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
-type entrant = {id:int; mph:float}
-type race = {entrant:entrant; times:seq<System.TimeSpan>}
-type cleanRead = {bib:int; checkpoint:int; time:System.TimeSpan}
+type entrant = 
+    { id : int
+      mph : float }
+
+type race = 
+    { entrant : entrant
+      times : seq<TimeSpan> }
+
+type cleanRead = 
+    { bib : int
+      checkpoint : int
+      time : TimeSpan }
 
 let anEighth = 1.0 / 8.0
 let aQuarter = 0.25
 let anHour = 60.0
 
-let toEntrant n =
-    let rnd = new System.Random(n)
-    { id = n; mph = float(rnd.Next(2,10)) + (rnd.NextDouble() * 2.0 - 1.0)}
+let toEntrant n = 
+    let rnd = new Random(n)
+    { id = n
+      mph = float (rnd.Next(2, 10)) + (rnd.NextDouble() * 2.0 - 1.0) }
 
-let toRace checkpoints e =
-    let rnd = new System.Random(e.id)
-    let times = seq{for i in 0..checkpoints do  
-                        yield float(i) * ((anHour / e.mph) + ((rnd.NextDouble() * aQuarter) - anEighth))
-                   }
-                |> Seq.map System.TimeSpan.FromMinutes
-    {entrant=e; times= times}
+let toRace checkpoints e = 
+    let rnd = new Random(e.id)
+    
+    let times = 
+        seq { 
+            for i in 0..checkpoints do
+                yield float (i) * ((anHour / e.mph) + ((rnd.NextDouble() * aQuarter) - anEighth))
+        }
+        |> Seq.map TimeSpan.FromMinutes
+    { entrant = e
+      times = times }
 
-let generate checkpoints entrants =
+let generate checkpoints entrants = 
     let toRaceWithCheckpoints = toRace checkpoints
-    [0 .. entrants]
+    { 0..entrants }
     |> Seq.map toEntrant
     |> Seq.map toRaceWithCheckpoints
+    |> Seq.map (fun i -> 
+           i.times |> Seq.mapi (fun idx j -> 
+                          { bib = i.entrant.id
+                            checkpoint = idx
+                            time = j }))
+    |> Seq.collect (fun f -> f)
 
 let generateSimple = generate 3
 
 [<EntryPoint>]
 let main argv = 
     match argv with
-    | p when p.Length = 2 -> generate (System.Convert.ToInt32(p.[1])) (System.Convert.ToInt32(p.[0]))
-    | p when p.Length = 1 -> generateSimple (System.Convert.ToInt32(p.[0]))
-    | _ -> printfn "expected: #entrants #checkpoints"  
-           Seq.empty<race>
-    |> Seq.map (fun i -> i.times 
-                            |> Seq.mapi (fun idx j -> {bib=i.entrant.id; checkpoint=idx; time=j;}) 
-                )
-    |> Seq.collect (fun f -> f)
+    | args when args.Length = 2 -> generate (Convert.ToInt32(args.[1])) (Convert.ToInt32(args.[0]))
+    | args when args.Length = 1 -> generateSimple (Convert.ToInt32(args.[0]))
+    | _ -> 
+        printfn "usage: (# of entrants) (# of checkpoints) [-w (file)]"
+        printfn "\t #1: (required) positional number of entrants"
+        printfn "\t #2: (required) positional number of checkpoints"
+        printfn "\t -t: [optional] print timing info"
+        printfn "\t -w: [optional] write to file"
+        printfn "expected: #entrants #checkpoints"
+        Seq.empty<cleanRead>
     |> Seq.sortBy (fun f -> f.time)
-    |> Seq.map Newtonsoft.Json.JsonConvert.SerializeObject
+    |> Seq.map JsonConvert.SerializeObject
     |> Seq.iter (fun f -> printfn "%s" f)
     |> ignore
     0

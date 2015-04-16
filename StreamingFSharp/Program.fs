2cd803031430333d968fa0c25f3a9489c57fe32a
Astn <austin.w.harris@gmail.com>
2015-04-16 16:58:46 -0600

    minor refactoring

--- StreamingFSharp/Program.fs
+++ StreamingFSharp/Program.fs
@@ -28,27 +28,34 @@ type outputRecord = {   groupName : string
                         age : int
                     }
 
-let groupReads (reads:IObservable<chipRead.Root>) = 
-    let overall = reads 
-                    |> Observable.groupBy (fun item -> item.Checkpoint)
-                    |> Observable.bind (fun group -> group 
-                                                        |> Observable.map (fun item-> (sprintf "Overall Checkpoint %i" group.Key ,item)))
-    let gender = reads 
-                    |> Observable.groupBy (fun item -> (item.Checkpoint,item.Gender))
-                    |> Observable.bind (fun group -> group
-                                                        |> Observable.map (fun item-> 
-                                                        let checkpoint, gender = group.Key
-                                                        (sprintf "%s Checkpoint %i" gender checkpoint ,item)))
-    let genderAge = reads 
-                    |> Observable.groupBy (fun item -> (item.Checkpoint, item.Gender, (item.Age+2)/5))
-                    |> Observable.bind (fun group -> group 
-                                                        |> Observable.map (fun item-> 
-                                                        let chk, gender, ageGroup = group.Key
-                                                        let ageRange = sprintf "%i-%i" (ageGroup * 5 - 2) ((ageGroup + 1) * 5 - 2)
-                                                        (sprintf "%s %s Checkpoint %i" gender ageRange chk ,item)))
-    overall
-        |> Observable.merge gender
-        |> Observable.merge genderAge
+let overallReads (reads:IObservable<chipRead.Root>) =
+    reads 
+        |> Observable.groupBy (fun item -> item.Checkpoint)
+        |> Observable.bind (fun group -> group 
+                                            |> Observable.map (fun item-> (sprintf "Overall Checkpoint %i" group.Key ,item)))
+
+let genderReads  (reads:IObservable<chipRead.Root>) =
+    reads 
+        |> Observable.groupBy (fun item -> (item.Checkpoint,item.Gender))
+        |> Observable.bind (fun group -> group
+                                            |> Observable.map (fun item-> 
+                                            let checkpoint, gender = group.Key
+                                            (sprintf "%s Checkpoint %i" gender checkpoint ,item)))
+
+let genderAgeReads  (reads:IObservable<chipRead.Root>) =
+    reads 
+        |> Observable.groupBy (fun item -> (item.Checkpoint, item.Gender, (item.Age+2)/5))
+        |> Observable.bind (fun group -> group 
+                                            |> Observable.map (fun item-> 
+                                            let chk, gender, ageGroup = group.Key
+                                            let ageRange = sprintf "%i-%i" (ageGroup * 5 - 2) ((ageGroup + 1) * 5 - 2)
+                                            (sprintf "%s %s Checkpoint %i" gender ageRange chk ,item)))
+
+
+let groupAllReads (reads:IObservable<chipRead.Root>) = 
+    reads |> overallReads
+    |> Observable.merge (reads |> genderReads)
+    |> Observable.merge (reads |> genderAgeReads)
 
 let stream input = 
         input
@@ -56,7 +63,7 @@ let stream input =
             |> Observable.ofSeqOn TaskPoolScheduler.Default
             |> Observable.publish
             |> Observable.refCount
-            |> groupReads  
+            |> groupAllReads  
 
 [<EntryPoint>]
 let main argv = 
@@ -68,7 +75,6 @@ let main argv =
     let mre = new ManualResetEvent(false)
     let printer = lines
                     |> stream
-                    |> Observable.timeoutSpan (TimeSpan.FromMilliseconds 3000.0)
                     |> Observable.map (fun (group,item) -> {groupName=group; bib=item.Bib; time= item.Time.TimeOfDay; age= item.Age;}  )
                     |> Observable.subscribeWithCallbacks  (fun item -> printfn "%s" <| JsonConvert.SerializeObject item ) 
                                                           (fun err -> printfn "Error: %A" err)

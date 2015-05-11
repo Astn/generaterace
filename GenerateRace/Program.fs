29c0390da1bd06546655688c48bf34e783bca6e1
Astn <austin.w.harris@gmail.com>
2015-05-11 15:05:45 -0600

    Updated GenerateRace to do basic real-time streaming.

--- GenerateRace/Program.fs
+++ GenerateRace/Program.fs
@@ -1,6 +1,8 @@
 ﻿open Newtonsoft.Json
 open System
-
+open FSharp.Control.Reactive
+open System.Reactive.Linq
+open System.Reactive.Joins
 // Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
@@ -13,7 +15,7 @@ type entrant =
 
 type race = 
     { entrant : entrant
-      times : seq<TimeSpan> }
+      times : IObservable<TimeSpan> }
       
 type cleanRead = 
     { bib : int
@@ -25,11 +27,13 @@ type cleanRead =
 let anEighth = 1.0 / 8.0
 let aQuarter = 0.25
 let anHour = 60.0
+let nowDt = System.DateTime.UtcNow
+let now = new System.TimeSpan( nowDt.Ticks )
 
 let toEntrant n = 
     let rnd = new Random(n)
     { id = n
-      startAt = (rnd.NextDouble() * 5.0) + (rnd.NextDouble() * 5.0) |> TimeSpan.FromMinutes
+      startAt = System.DateTime.Now.TimeOfDay
       mph = float (rnd.Next(2, 10)) + (rnd.NextDouble() * 2.0 - 1.0) 
       age = rnd.Next(10,70)
       gender = match rnd.NextDouble() with
@@ -37,47 +41,56 @@ let toEntrant n =
                 | _ -> "female"}
 
 let toRace checkpoints e = 
+
     let rnd = new Random(e.id)
-    
-    let times = 
-        seq { 
-            for i in 0..checkpoints do
-                yield float (i) * ((anHour / e.mph) + ((rnd.NextDouble() * aQuarter) - anEighth))
-        }
-        |> Seq.map TimeSpan.FromMinutes
-        |> Seq.map (fun ct -> ct + e.startAt)
-    { entrant = e
-      times = times }
+    let times2 = 
+        Observable.Generate (0, 
+            (fun i -> i < checkpoints), 
+            (fun i -> i + 1), 
+            (fun i -> float (i) * ((anHour / e.mph) + ((rnd.NextDouble() * aQuarter) - anEighth))),
+            (fun i -> 
+                let j = float (i) * ((anHour / e.mph) + ((rnd.NextDouble() * aQuarter) - anEighth))
+                new DateTimeOffset((now + System.TimeSpan.FromMinutes(j)).Ticks, System.TimeSpan.FromSeconds(0.)))
+                )
+        |> Observable.map TimeSpan.FromMinutes
+        |> Observable.map (fun ct -> ct + e.startAt)
+    times2
 
-let generate checkpoints entrants = 
-    let toRaceWithCheckpoints = toRace checkpoints
-    { 0..entrants }
-    |> Seq.map toEntrant
-    |> Seq.map toRaceWithCheckpoints
-    |> Seq.map (fun i -> 
-           i.times |> Seq.mapi (fun idx j -> 
-                          { bib = i.entrant.id
-                            checkpoint = idx
-                            gender = i.entrant.gender
-                            age = i.entrant.age
-                            time = j }))
-    |> Seq.collect (fun f -> f)
-
-let generateSimple = generate 3
+let generate (checkpoints:int) (entrants:int) : IObservable<cleanRead> = 
+    Observable.Generate ( 0,
+            (fun i -> i < entrants),
+            (fun i -> i + 1),
+            (fun i -> i ),
+            (fun i -> 
+                      let rnd = new Random(i) 
+                      let rndOff = (float i * 0.25) + (rnd.NextDouble() * 5.) |> TimeSpan.FromSeconds
+                      new DateTimeOffset((nowDt + rndOff), System.TimeSpan.FromSeconds(0.))))
+    |> Observable.map toEntrant
+    |> Observable.map (fun entrant -> {entrant = entrant; times = toRace checkpoints entrant})
+    |> Observable.map (fun i -> 
+                           i.times 
+                           |> Observable.scanInit (0, System.TimeSpan.FromMinutes(0.)) (fun u t -> (fst u + 1,t)) 
+                           |> Observable.map (fun (idx,j) -> 
+                                  { bib = i.entrant.id
+                                    checkpoint = idx
+                                    gender = i.entrant.gender
+                                    age = i.entrant.age
+                                    time = j }))
+    |> Observable.flatmap (fun f -> f)
 
 [<EntryPoint>]
 let main argv = 
     match argv with
     | args when args.Length = 2 -> generate (Convert.ToInt32(args.[1])) (Convert.ToInt32(args.[0]))
-    | args when args.Length = 1 -> generateSimple (Convert.ToInt32(args.[0]))
+    | args when args.Length = 1 -> generate (Convert.ToInt32(args.[0])) 3
     | _ -> 
         printfn "usage: (# of entrants) (# of checkpoints)"
         printfn "\t #1: (required) positional number of entrants"
         printfn "\t #2: (required) positional number of checkpoints"
         printfn "expected: #entrants #checkpoints"
-        Seq.empty<cleanRead>
-    |> Seq.sortBy (fun f -> f.time)
-    |> Seq.map JsonConvert.SerializeObject
-    |> Seq.iter (fun f -> printfn "%s" f)
+        Observable.empty<cleanRead>
+    |> Observable.map JsonConvert.SerializeObject
+    |> Observable.subscribeWithCallbacks (fun f -> printfn "%s" f) (fun e -> printfn "%A" e) (fun () -> printfn "done")
     |> ignore
+    Console.ReadLine() |> ignore
     0
